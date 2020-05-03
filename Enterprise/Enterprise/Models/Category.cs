6a58b671733bda868e8a8cfb966663d83ab82407
Clint <58631855+buzu99@users.noreply.github.com>
2020-05-03 14:55:33 +0200

    category model implemented

---
+++ Enterprise/Enterprise/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Enterprise.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Categoy name is too short")]
+        public string Name { get; set; }
+
+    }
+}
