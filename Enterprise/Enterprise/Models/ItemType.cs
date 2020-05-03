7e8cefa351cb7e04927e282420c5430c0e314db1
Clint <58631855+buzu99@users.noreply.github.com>
2020-05-03 15:01:11 +0200

    item type model implemented

---
+++ Enterprise/Enterprise/Models/ItemType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Enterprise.Models
+{
+    public class ItemType
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage ="Your type name is short")]
+        public string name { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage ="image url cannot be that short")]
+        public string image { get; set; }
+
+    }
+}
