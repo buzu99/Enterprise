7cfb562ee0edb1a412963961a7f3300f9fa85081
Clint <58631855+buzu99@users.noreply.github.com>
2020-05-03 12:50:53 +0200

    initial commit

---
+++ Enterprise/Enterprise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Enterprise.Startup))]
+namespace Enterprise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
