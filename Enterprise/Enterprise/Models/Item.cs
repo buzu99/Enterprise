d20bf6d31d08b151413808bfa7a840fb995cd84b
Clint <58631855+buzu99@users.noreply.github.com>
2020-05-03 15:13:30 +0200

    items model has been implemented

---
+++ Enterprise/Enterprise/Models/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Enterprise.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Key]
+        public int TypeId { get; set; }
+
+        [Required]
+        [Range(1.0, 1000.0,ErrorMessage = "cannot sell an items at a quantity more than 1000")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [MinLength(2,ErrorMessage = "text too short")]
+        public string Quality { get; set; }
+
+        [Required]
+        [Range(1.0, 1000000.0, ErrorMessage = "the price of an item must be between 1 and 1000000")]
+        public int Price { get; set; }
+
+        [Key]
+        public int UserId { get; set; }
+
+    }
+}
